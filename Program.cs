144c823be3409a1cac563c2001a5c77419d0073f
gofffro <vazheninstepan@gmail.com>
2025-06-27 11:22:37 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+
+namespace iterator
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      MyCollection collection = new MyCollection();
+      collection.Add("Первый");
+      collection.Add("Второй");
+      collection.Add("Третий");
+
+      IIterator iterator = collection.GetIterator();
+
+      while (iterator.HasNext())
+      {
+        Console.WriteLine(iterator.Next());
+      }
+    }
+  }
+
+  public interface IIterator
+  {
+    bool HasNext();
+    object Next();
+  }
+
+  public interface IAggregate
+  {
+    IIterator GetIterator();
+  }
+
+  public class MyCollection : IAggregate
+  {
+    private ArrayList items = new ArrayList();
+
+    public void Add(object item)
+    {
+      items.Add(item);
+    }
+
+    public IIterator GetIterator()
+    {
+      return new MyIterator(this);
+    }
+
+    public int Count()
+    {
+      return items.Count;
+    }
+
+    public object Get(int index)
+    {
+      return items[index];
+    }
+  }
+  public class MyIterator : IIterator
+  {
+    private MyCollection collection;
+    private int position = 0;
+
+    public MyIterator(MyCollection collection)
+    {
+      this.collection = collection;
+    }
+
+    public bool HasNext()
+    {
+      return position < collection.Count();
+    }
+
+    public object Next()
+    {
+      return collection.Get(position++);
+    }
+  }
+}
